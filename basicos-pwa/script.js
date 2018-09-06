3be13b4badcfd2b820d571a740d67383e8999cea
ema18787 <emanueljtc@gmail.com>
2018-09-06 14:49:35 -0400

    cambios

--- basicos-pwa/script.js
+++ basicos-pwa/script.js
@@ -1,15 +1,38 @@
-if( 'serviceWorker' in navigator ) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-    .then( registration => {
-      console.log(registration)
-      console.log(
-        'Services Worker Registrado con Exito',
-        registration.scope
-      );
+;
+//  Registro de Caracteristicas de PWA's
+((d, w, n, c) => {
+  // Registro Service Workers
+  if ('serviceWorker' in n) {
+    w.addEventListener('load', () => {
+      n.serviceWorker.register('./sw.js')
+        .then(registration => {
+          c(registration)
+          c(
+            'Services Worker Registrado con Exito',
+            registration.scope
+          );
+        })
+        .catch(
+          err => c(`Registro de Service Worker Fallido`, err)
+        )
     })
-    .catch(
-      err => console.log(`Registro de Service Worker Fallido`, err)
-    )
-  })
-}
+  }
+  // Activar servicio de las Notificaciones 
+  if (w.Notification && Notification.permission !== 'denied') {
+    Notification.requestPermission(status => {
+      console.log(status)
+      let n = new Notification('Titulo', {
+        body: 'Soy una notificación',
+        icon: './img/icon_192x192.png' // opcional
+      })
+    })
+  }
+})(document, window, navigator, console.log);
+
+((d, w, n, c) => {
+
+})(document, window, navigator, console.log);
+
+((d, w, n, c) => {
+
+})(document, window, navigator, console.log);
